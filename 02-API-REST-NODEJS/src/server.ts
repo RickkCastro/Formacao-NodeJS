7adf84d2789113f5dc205f43779864108b4efd05
rickkcastro <Heinrickoficial25@gmail.com>
2025-01-11 23:48:28 -0300

    update

--- 02-API-REST-NODEJS/src/server.ts
+++ 02-API-REST-NODEJS/src/server.ts
@@ -5,5 +5,5 @@ app.listen({
 	port: env.PORT,
 	host: 'RENDER' in process.env ? '0.0.0.0' : 'localhost',
 }).then(() => {
-	console.log('Server is running on http://localhost:3333');
+	console.log(`Server is running on http://localhost:${env.PORT}`);
 });
